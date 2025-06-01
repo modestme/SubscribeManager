8854d7fa2dc407fa029fda5219ae8cd980c1e43c
QYC <844713856@qq.com>
2025-06-01 13:52:54 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="subscribe-manager",
-    version="0.1.0",
+    version="0.2.0",
     author="modestme",
     author_email="844713856@qq.com",
     description="A subscription proxy tool that unifies subscription management, regularly downloads and stores "
